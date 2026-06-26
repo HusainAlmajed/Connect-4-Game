9c6003c78b9e755221e922ed8ee6f5d05c137375
Husain <hussainalmajed.2005@gmail.com>
2026-06-26 21:37:38 +0300

    I've added a reference for the board and circles so, now I can specify each circles id when clicked

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
 
+
+
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
+const board = document.querySelector('.board');
+const circles = document.querySelectorAll('.crcl');
 
 /*-------------------------------- Functions --------------------------------*/
+const getId = (circle) => {
+    console.log('Hello, Im Circle')
+};
+/*----------------------------- Event Listeners -----------------------------*/
+circles.forEach((circle) => {
+circle.addEventListener ('click' , function() {
+console.log(`Hi I'm circle ${circle.id}`)
+})
+});
