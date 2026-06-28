f24014f49266fd541d50c7aa6ef29b1d56785abe
Husain <hussainalmajed.2005@gmail.com>
2026-06-28 21:52:23 +0300

    A message will bes displayed stating whos current turn to play

--- app.js
+++ app.js
@@ -12,35 +12,51 @@ const board = document.querySelector('.board');
 const circles = document.querySelectorAll('.crcl');
 const butn = document.querySelector('button');
 const head = document.querySelector('h1');
+const msg = document.querySelector('h2');
 
 /*-------------------------------- Functions --------------------------------*/
+const changeMessage = (circle) => {
+    
+    if (circle.style.backgroundColor !== '') {
+        if (turn === 'Blue') {
+            msg.textContent = `It's Blue's Turn`;
+        } else if (turn === 'Yellow') {
+            msg.textContent = `It's Yellow's Turn`;
+        }
+    }
+    console.log('cuurent turn: ' , turn)
+};
+
 const getId = (circle) => {
-    console.log('Hello, Im Circle')
+    console.log('Hello, Im Circle');
 };
 
 const changeColor = (circle) => {
+    if (circle.style.backgroundColor === '') {
 
     circle.style.backgroundColor = turn;
+    changeTurn();
+    }
 
 }
 
 const changeTurn = () => {
 
     if (turn === 'Blue') turn = 'Yellow';
-    else turn = 'Blue'
+    else turn = 'Blue';
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
 circles.forEach((circle) => {
 circle.addEventListener ('click' , function() {
-console.log(turn)
-console.log(`Hi I'm circle ${circle.id}`)
-changeTurn();
-console.log(turn)
+console.log(turn);
+console.log(`Hi I'm circle ${circle.id}`);
+console.log(turn);
 changeColor(circle);//We need to pass an input for the function
+changeMessage(circle);// we change the message after changing turns
 
 })});
 
-butn.addEventListener('click' , function(){
+butn.addEventListener ('click' , function() {
 circles.style.backgroundColor = 'Blue';
 });
